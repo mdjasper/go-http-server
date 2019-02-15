4adda35fd8f36e9176b77cf294bf3f113ff39c8d
Michael Jasper <mjasper@instructure.com>
2019-02-15 13:12:26 -0700

    comments

--- main.go
+++ main.go
@@ -8,7 +8,8 @@ import (
 )
 
 func main() {
-	service := ":1200"
+	// Open a TCP connection on a specified port
+	service := ":1234"
 	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
 	fmt.Println("listening on: ", tcpAddr)
 
@@ -18,9 +19,13 @@ func main() {
 		fmt.Println("Could not open TCP connection", err)
 	}
 
-	// close the tcp listener when that application closes
+	// close the tcp listener when the application closes
 	defer listener.Close()
 
+	// Main server loop
+	// * listen for a connection
+	// * generate and write responses
+	// * close the connection
 	for {
 		conn, err := listener.Accept() // Blocking. Waits for connection
 		if err != nil {
@@ -28,21 +33,23 @@ func main() {
 			continue
 		}
 
+		// Handle incoming connections in a thread
 		go func() {
 			// Read connection into a byte array
-			// and convert into a Request
 			buffer := make([]byte, 1024)
 			reqLen, err := conn.Read(buffer)
 			if err != nil {
 				fmt.Println("Error reading from connection", err)
 			}
 
-			//
+			// Transform the request byte array into a map of headers
 			requestString := strings.TrimSpace(string(buffer[:reqLen]))
 			request := MakeRequestFromString(requestString)
+			// Log the incoming request
 			fmt.Printf("\033[1;36m%s\033[0m", time.Now().Format("2006-01-02 15:04:05")+" [Go Server] ")
 			fmt.Printf("%+v\n", request)
 
+			// Generate and write a response, and close the connection
 			responseString := router(request)(request)
 
 			conn.Write([]byte(responseString))
