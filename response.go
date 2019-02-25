ea7ccbef92b8168640bace80f69c68cac6e95d47
Michael Jasper <mjasper@instructure.com>
2019-02-25 16:12:28 -0700

    comments

--- response.go
+++ response.go
@@ -9,7 +9,7 @@ func response(status int, headers Headers, body string) string {
 
 	return "HTTP/1.1 " +
 		HttpStatusCodes[status] +
-		"\r\n" + headers.String() +
+		"\r\n" + headers.String() + // headers.String() includes a trailing "\r\n"
 		"\r\n" +
 		body
 }
